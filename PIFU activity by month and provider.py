26c2f6386bad2e7a58f40b3e81f11d90e2f69659
A-bulteel <A-bulteel>
2025-03-06 16:39:58 +0000

    Adding existing code

---
+++ PIFU activity by month and provider.py
@@ -0,0 +1,62 @@
+# Databricks notebook source
+# Data connection settings
+# Blob storage container
+#DFS URL
+#Set connection settings for PIFU data
+reportingLake=""
+reportingContainer=""
+PIFUPath=""
+PIFUPath="abfss://"+reportingContainer+"@"+reportingLake+PIFUPath
+
+#Load PIFU data
+raw_PIFU = spark.read.option("header","true").option("recursiveFileLookup","true").parquet(PIFUPath)
+#Temp view holding PIFU table data
+raw_PIFU.createOrReplaceGlobalTempView ("RawPIFU")
+
+;
+
+query = """
+select * from global_temp.RawPIFU
+"""
+df = spark.sql(query)
+display(df)
+
+# COMMAND ----------
+
+# MAGIC %sql
+# MAGIC SHOW TABLES IN global_temp;
+
+# COMMAND ----------
+
+# MAGIC %sql
+# MAGIC  
+# MAGIC SELECT 
+# MAGIC        `EROC_DerMonth`
+# MAGIC 	  ,`EROC_DerProviderCode`
+# MAGIC       ,`EROC_DerProviderName`
+# MAGIC       ,`EROC_DerRegionName`
+# MAGIC       ,`EROC_DerRegionCode`
+# MAGIC       ,`EROC_DerICBCode`
+# MAGIC       ,`EROC_DerICBName`
+# MAGIC       ,`EROC_DerProviderAcuteStatus`
+# MAGIC        
+# MAGIC 	 --,`EROC_DerProviderAcuteStatus`
+# MAGIC 	 --`EROC_DerRegionCode`
+# MAGIC      --,`EROC_DerRegionName`
+# MAGIC      --,`EROC_DerICBCode`
+# MAGIC 	 -- ,`EROC_DerICBName`
+# MAGIC       ,sum(`EROC_Value`) as `Moved_or_Discharged` 
+# MAGIC   FROM `global_temp`.`RawPIFU`
+# MAGIC   where `EROC_DerMetricReportingName` = 'Moved and Discharged'
+# MAGIC   and   `EROC_DerMonth` >'2021-03-01'
+# MAGIC   --For ICB/Region filters, remove the filter for 'Acute' providers, and include the relevant ICB/Region fields for aggregation. 
+# MAGIC   group by 
+# MAGIC        `EROC_DerProviderCode`
+# MAGIC       ,`EROC_DerProviderName`
+# MAGIC       ,`EROC_DerMonth`
+# MAGIC       ,`EROC_DerRegionName`
+# MAGIC       ,`EROC_DerRegionCode`
+# MAGIC       ,`EROC_DerICBCode`
+# MAGIC       ,`EROC_DerICBName`
+# MAGIC       ,`EROC_DerProviderAcuteStatus` 
+# MAGIC 	 order by `EROC_DerMonth`
