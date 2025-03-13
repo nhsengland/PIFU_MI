5b5ed23dffba2cc54f807249c0335335c660fe1c
A-bulteel <A-bulteel>
2025-03-13 14:16:13 +0000

    Converted to Pyspark

--- PIFU activity by month and provider.py
+++ PIFU activity by month and provider.py
@@ -1,12 +1,14 @@
 # Databricks notebook source
 from env import env
 
+from pyspark.sql import functions as F
+
 # COMMAND ----------
 
 #Load PIFU data
-raw_PIFU = spark.read.option("header","true").option("recursiveFileLookup","true").parquet(env["pifu_path"])
+df_raw_pifu = spark.read.option("header","true").option("recursiveFileLookup","true").parquet(env["pifu_path"])
 #Temp view holding PIFU table data
-raw_PIFU.createOrReplaceGlobalTempView("RawPIFU")
+df_raw_pifu.createOrReplaceGlobalTempView("RawPIFU")
 
 
 query = """
@@ -53,4 +55,48 @@ display(df)
 # MAGIC       ,`EROC_DerICBCode`
 # MAGIC       ,`EROC_DerICBName`
 # MAGIC       ,`EROC_DerProviderAcuteStatus` 
-# MAGIC 	 order by `EROC_DerMonth`
+# MAGIC 	 order by EROC_DerMonth, EROC_DerProviderCode
+
+# COMMAND ----------
+
+df_processed_pifu = (df_raw_pifu
+    .where ( F.col("EROC_DerMetricReportingName") == "Moved and Discharged")
+    .where (F.col("EROC_DerMonth") > '2021-03-01')
+    .groupby(
+        "EROC_DerMonth",
+	    "EROC_DerProviderCode",
+        "EROC_DerProviderName",
+        "EROC_DerRegionName",
+        "EROC_DerRegionCode",
+        "EROC_DerICBCode",
+        "EROC_DerICBName",
+        "EROC_DerProviderAcuteStatus"
+    )
+    .agg (F.sum("EROC_Value").alias("Moved_or_Discharged"))
+    .orderBy(
+        "EROC_DerMonth",
+        "EROC_DerProviderCode"
+    )
+)
+       
+# 	 --,`EROC_DerProviderAcuteStatus`
+# 	 --`EROC_DerRegionCode`
+#      --,`EROC_DerRegionName`
+#      --,`EROC_DerICBCode`
+# 	 -- ,`EROC_DerICBName`
+#       ,sum(`EROC_Value`) as `Moved_or_Discharged` 
+#   FROM `global_temp`.`RawPIFU`
+#   where `EROC_DerMetricReportingName` = 'Moved and Discharged'
+#   and   `EROC_DerMonth` >'2021-03-01'
+#   --For ICB/Region filters, remove the filter for 'Acute' providers, and include the relevant ICB/Region fields for aggregation. 
+#   group by 
+#        `EROC_DerProviderCode`
+#       ,`EROC_DerProviderName`
+#       ,`EROC_DerMonth`
+#       ,`EROC_DerRegionName`
+#       ,`EROC_DerRegionCode`
+#       ,`EROC_DerICBCode`
+#       ,`EROC_DerICBName`
+#       ,`EROC_DerProviderAcuteStatus` 
+# 	 order by `EROC_DerMonth`
+display (df_processed_pifu)
